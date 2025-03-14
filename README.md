b21a8c2e0bd08dff7932ae7a0ccbd63e052103ad
ddonamaria <daviddonamaria@gmail.com>
2025-03-14 21:51:35 +0100

    First commit - Basic Java app created with Maven

--- README.md
+++
@@ -1,2 +0,0 @@
-# test-book-filter
-[Prueba técnica] - Servicio de filtrado y procesamiento de libros.
---
+++ src/main/java/com/ddu/bookfilter/BookFilterApp.java
@@ -0,0 +1,7 @@
+package com.ddu.bookfilter;
+
+public class BookFilterApp {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a BookFilterApp");
+    }
+} 
